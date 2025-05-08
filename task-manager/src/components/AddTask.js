7fd8052e74b8da44bdb191c03b50038bfc2df026
rai8d4 <122903297+rai8d4@users.noreply.github.com>
2025-05-08 12:20:10 -0500

    Edited AddTask component to use actual task addition logic

--- task-manager/src/components/AddTask.js
+++ task-manager/src/components/AddTask.js
@@ -1,28 +1,100 @@
-import React from 'react'
-import '../App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import '../App.css';
 
-// AddTask component
-// This component will be used to add a new task
+
 function AddTask() {
-  return (
-    <div>
-            <h2>Add Task</h2>
-            <div>
-                <label>
-                    Task Name:
-                    <input type="text" name="taskName" />
-                </label>
-                <br />
-                <label>
-                    Description:
-                    <textarea name="description"></textarea>
-                </label>
-                <br />
-                <button id="addTask">Add Task</button>
-            </div>
-        </div>
+
+  const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+  const [date, setDate] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+
+    const newTask = {
+      taskName,
+      taskDescription,
+      date
+
+    };
+
+    try {
+
+      //temporarily hardcoding a board, if time permits, change to real board later
+      const res = await axios.get('http://localhost:5000/api/boards');
+      
+
+      const firstBoard = res.data[0];
+
+      //if no board exist then alert user
+      //eventual idea of 3 board: To-Do, In-Progress, Done
+      if (!firstBoard || !firstBoard.boards[0]) {
+        alert('No board available to add task');
+        return;
+      }
+
+      // Add to first column 
+      firstBoard.boards[0].items.push(newTask);
+
+      await axios.put('http://localhost:5000/api/boards', {
+
+        name: firstBoard._id,
+        ...firstBoard
+      
+    });
+
+      alert('Task added!');
+      navigate('/');
     
-  )
+    } 
+    
+    catch (err) {
+      console.error(err);
+      alert('Failed to add task');
+    }
+
+  };
+
+
+  return (
+
+    <div>
+
+      <h2>Add Task</h2>
+
+      <div>
+
+        <label>
+          Task Name:
+          <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+        </label>
+
+        <br />
+
+        <label>
+          Description:
+          <textarea value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)}></textarea>
+        </label>
+
+        <br />
+
+        <label>
+          Due Date:
+          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+        </label>
+
+        <br />
+
+        <button onClick={handleSubmit}>Add Task</button>
+
+      </div>
+
+    </div>
+    
+  );
 }
 
-export default AddTask
+export default AddTask;
